30e4d4969bc950b30236df1dfdfcf40e59d0c8c5
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-11-20 00:27:49 +0545

    short circuit evaluation

--- src/components/ShortCircuit.jsx
+++ src/components/ShortCircuit.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ShortCircuit = () => {
+  const [demo, setDemo] = useState("");
+
   return (
     <>
-      <h1>hello from shortcircuit</h1>
+      <h1>
+        {demo || (
+          <>
+            <h1>mast</h1>
+            <p>to can do anything</p>
+          </>
+        )}
+      </h1>
+      <h1>{demo && "anish"} </h1>
     </>
   );
 };
