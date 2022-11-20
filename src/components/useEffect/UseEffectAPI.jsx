985a5cccc02e17faefa41e38589fbaf347dbfd4d
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-11-20 19:45:25 +0545

    errorhandling

--- src/components/useEffect/UseEffectAPI.jsx
+++ src/components/useEffect/UseEffectAPI.jsx
@@ -1,58 +1,35 @@
 import React, { useEffect, useState } from "react";
+import Githubuser from "./github/Githubuser";
+import Loading from "./github/Loading";
 
 const UseEffectAPI = () => {
   const [users, setUsers] = useState([]);
 
+  const [loading, setLoading] = useState(true);
+
   const getUsers = async () => {
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/albums/1/photos"
-    );
-    setUsers(await response.json());
+    try {
+      setLoading(false);
+
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/albums/1/photos"
+      );
+      setUsers(await response.json());
+    } catch (error) {
+      console.log("my error is " + error);
+    }
   };
   useEffect(() => {
     getUsers();
   }, []);
 
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <>
-      <h2>List of gitHub user</h2>
-      <div className="container-fluid mt-5">
-        <div className="row text-center">
-          {users.map((curElem) => {
-            const { title, id, thumbnailUrl } = curElem;
-
-            return (
-              <div iv className="col-10 col-md-4 mt-5" key={id}>
-                <div className="card p-2">
-                  <div className="d-flex align-items-center">
-                    <div className="image">
-                      <img src={thumbnailUrl} className="rounded" width="155" />
-                    </div>
-                    <div className="ml-3 w-100">
-                      <h4 className="mb-0 mt-0 textLeft">Anish</h4>
-                      <span className="text-left">{title}</span>
-                      <div className="p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats">
-                        <div className="d-flex flex-column">
-                          <span className="articles">Articles</span>
-                          <span className="number1">38</span>
-                        </div>
-                        <div className="d-flex flex-column">
-                          <span className="followers">Followers</span>
-                          <span className="number2">980</span>
-                        </div>
-                        <div className="d-flex flex-column">
-                          <span className="rating">Rating</span>
-                          <span className="number3">8.9</span>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
+      <Githubuser users={users} />
     </>
   );
 };
